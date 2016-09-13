f2fbf0dced7cbfc316e00a09aef6b4351279eedd
David Ladowitz <david@ladowitz.com>
2016-09-13 13:28:31 -0700

    finished rna transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,4 +1,20 @@
+module BookKeeping
+  VERSION = 4 # Where the version number matches the one in the test.
+end
+
+
 class Complement
-  def Complement.of_dna(argument1)
+  TRANSCRIBER = {"G" => "C", "C" =>"G", "T" => "A", "A" => "U"}
+
+  def Complement.of_dna(dna_strand)
+    rna_strand = dna_strand.split("").map do |nucleotide|
+      if TRANSCRIBER[nucleotide]
+        TRANSCRIBER[nucleotide]
+      else
+        return ""
+      end
+    end
+
+    rna_strand.join("")
   end
 end
